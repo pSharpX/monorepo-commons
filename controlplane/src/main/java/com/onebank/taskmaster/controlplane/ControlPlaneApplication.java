e050a8333c772352d60b9fbd8733a2bbc0f65f3b
Christian Rivera <crivera2093@gmail.com>
2024-07-31 19:47:23 -0500

    feat: add server schema support.

--- controlplane/src/main/java/com/onebank/taskmaster/controlplane/ControlPlaneApplication.java
+++ controlplane/src/main/java/com/onebank/taskmaster/controlplane/ControlPlaneApplication.java
@@ -1,10 +1,13 @@
 package com.onebank.taskmaster.controlplane;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@OpenAPIDefinition(servers = {@Server(url = "/")})
 @EnableConfigurationProperties
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ControlPlaneApplication {
