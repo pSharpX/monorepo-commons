8041778a15339969a7bb6b414f710abb7b91f3a4
Christian Rivera <crivera2093@gmail.com>
2024-12-11 01:43:27 -0500

    feat(taskmaster-multipledb): set TYPE attribute in PubSubMessage to filter messages in Subscriptions.

--- taskmaster-multipledb/src/main/java/com/onebank/taskmaster/notifier/service/producers/PubSubNotificationMessageProducer.java
+++ taskmaster-multipledb/src/main/java/com/onebank/taskmaster/notifier/service/producers/PubSubNotificationMessageProducer.java
@@ -1,7 +1,11 @@
 package com.onebank.taskmaster.notifier.service.producers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.cloud.spring.pubsub.core.publisher.PubSubPublisherTemplate;
+import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.ProjectTopicName;
+import com.google.pubsub.v1.PubsubMessage;
 import com.onebank.taskmaster.notifier.async.ConditionalOnPubSubEnabled;
 import com.onebank.taskmaster.notifier.model.senders.NotificationMessage;
 import lombok.NonNull;
@@ -16,9 +20,21 @@ import org.springframework.stereotype.Service;
 public class PubSubNotificationMessageProducer implements NotificationMessageProducer {
     private final PubSubPublisherTemplate publisherTemplate;
     private final ProjectTopicName topicName;
+    private final ObjectMapper objectMapper;
+
+    private static final String TYPE_ATTRIBUTE = "type";
 
     @Override
     public void sendMessage(@NonNull NotificationMessage notification) {
-        publisherTemplate.publish(topicName.toString(), notification);
+        try {
+            PubsubMessage pubsubMessage = PubsubMessage.newBuilder()
+                    .setData(ByteString.copyFromUtf8(objectMapper.writeValueAsString(notification)))
+                    .putAttributes(TYPE_ATTRIBUTE, notification.getChannel().getName())
+                    .build();
+            publisherTemplate.publish(topicName.toString(), pubsubMessage);
+        } catch (JsonProcessingException e) {
+            log.error("Error occurred while publishing message");
+            throw new RuntimeException(e);
+        }
     }
 }
